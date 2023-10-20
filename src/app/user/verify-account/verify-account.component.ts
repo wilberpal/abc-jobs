e5298bea8fa230f6d1b052eacd4321cbba477966
Franz Joseph <frogelez@zenware.com.co>
2023-10-20 10:29:14 -0500

    Ajustes

--- src/app/user/verify-account/verify-account.component.ts
+++ src/app/user/verify-account/verify-account.component.ts
@@ -2,41 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
-const dummy=[{
-  nombre:"Estados Unidos",
+const dummy = [{
+  nombre: "Estados Unidos",
 },
 {
-  nombre:"Argentina",
+  nombre: "Argentina",
 },
 {
-  nombre:"Brazil",
+  nombre: "Brazil",
 },
 {
-  nombre:"Bolivia",
+  nombre: "Bolivia",
 },
 {
-  nombre:"Colombia",
+  nombre: "Colombia",
 },
 {
-  nombre:"Chile",
+  nombre: "Chile",
 },
 {
-  nombre:"Uruguay",
+  nombre: "Uruguay",
 },
 {
-  nombre:"Venezuela",
+  nombre: "Venezuela",
 },
 {
-  nombre:"Mexico",
+  nombre: "Mexico",
 },
 {
-  nombre:"Bolivia",
+  nombre: "Bolivia",
 },
 {
-  nombre:"Ecuador",
+  nombre: "Ecuador",
 },
 {
-  nombre:"Peru",
+  nombre: "Peru",
 },
 ];
 
@@ -45,69 +45,133 @@ const dummy=[{
   templateUrl: './verify-account.component.html',
   styleUrls: ['./verify-account.component.scss'],
 })
-export class VerifyAccountComponent  implements OnInit {
-  cellphone:number = 0;
+export class VerifyAccountComponent implements OnInit {
+  cellphone: any;
   country = "";
-  modeScreen:string = ''
-  dummy:any[]=dummy;
-  step:number;
-  activePhone:boolean;
-  activeMail:boolean;
-  countrySelected:any;
-  email:string=""
+  modeScreen: string = ''
+  dummy: any[] = dummy;
+  step: number;
+  activePhone: boolean;
+  activeMail: boolean;
+  countrySelected: any;
+  email: string = ""
+  flagImage: string = 'assets/image/colombia.webp';
+  currentPrefix: string = '+57';
+  otpDigits: string[] = ['', '', '', ''];
+  readonly totalDigits = 4;
+  timer: number = 300;
+  interval: any;
+  verifyTime:boolean=false
+
 
   constructor(private router: Router,
     private translate: TranslateService) {
     this.step = 0;
     this.activeMail = false;
     this.activePhone = false;
-   }
+  }
+
+  startTimer() {
+    this.timer = 30
+    this.verifyTime = true;
+    this.interval = setInterval(() => {
+      if (this.timer > 0) {
+        this.timer--;
+      } else {
+        this.stopTimer();
+      }
+    }, 1000)
+  }
+
+  stopTimer() {
+    clearInterval(this.interval);
+    this.verifyTime = false
+  }
+
+  formatTime(seconds: number): string {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${this.pad(mins)}:${this.pad(secs)}`;
+  }
+
+  pad(num: number): string {
+    return ('00' + num).slice(-2);
+  }
+
+  areAllFieldsFilled(): boolean {
+    return this.otpDigits.every(digit => digit.trim() !== '');
+  }
+
+  toggleCountry(): void {
+    if (this.currentPrefix === '+57') {
+      this.flagImage = 'path/to/usa-flag.png'; // Reemplaza con tu ruta real
+      this.currentPrefix = '+1';
+    } else {
+      this.flagImage = 'path/to/colombia-flag.png'; // Reemplaza con tu ruta real
+      this.currentPrefix = '+57';
+    }
+  }
+
 
   ngOnInit() {
     const lang = localStorage.getItem('appLang') || 'es';
     this.translate.use(lang);
     const Mode = localStorage.getItem('DarkMode') || 'light';
     this.modeScreen = Mode;
-    if(Mode === 'dark'){
+    if (Mode === 'dark') {
       document.body.classList.toggle('dark', true);
     }
   }
 
-  next(){
-    if(this.step >= 0 && this.step < 4){
+  next() {
+    if (this.step >= 0 && this.step < 4) {
       this.step += 1;
+    }
+    if (this.step == 3) {
+      this.startTimer();
+    } else {
+      this.stopTimer();
     }
   }
 
-  chooseCountry(dummie:any){
-    this.countrySelected =dummie;
+  chooseCountry(dummie: any) {
+    this.countrySelected = dummie;
   }
 
-  back(){
-    if(this.step >= 0 && this.step <= 4){
+  isEmailValid(email: string): boolean {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (email.trim() == "" || !regex.test(email)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  back() {
+    if (this.step >= 0 && this.step <= 4) {
       this.step -= 1;
     }
   }
 
-  activeVerify(method:string){
-    if(method =='phone'){
+  activeVerify(method: string) {
+    if (method == 'phone') {
       this.activePhone = true;
-      if(this.activeMail == true){
+      if (this.activeMail == true) {
         this.activeMail = false;
       }
     }
-    if(method =='mail'){
+    if (method == 'mail') {
       this.activeMail = true;
-      if(this.activePhone == true){
+      if (this.activePhone == true) {
         this.activePhone = false;
       }
     }
   }
 
-  sendToAccount(){
+  sendToAccount() {
     this.router.navigate(['/user/project']);
   }
-  sendToJobs(){
+  sendToJobs() {
     this.router.navigate(['/user/profile']);
   }
 
